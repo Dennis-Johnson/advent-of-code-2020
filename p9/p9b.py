2d497f3e782dd6b1ab29c03a6a73bf2b4d02d52d
Dennis Johnson <densarjohn@gmail.com>
2020-12-10 16:30:13 +0530

    day9b, ignore the second o/p line

--- p9/p9b.py
+++ p9/p9b.py
@@ -4,7 +4,7 @@ for line in open("input.txt", "r").readlines():
     num = line.strip().split()
     nums.append(int(num[0]))
 
-# window= 5
+#window= 5
 window = 25
 
 for i in range(window, len(nums)):
@@ -14,4 +14,19 @@ for i in range(window, len(nums)):
     if nums[i] in valid_sums:
         continue
     else: 
+        invalid = nums[i]
         print("{} is invalid".format(nums[i]))
+
+for low in range(len(nums)):
+    for high in range(low + 1, len(nums)):
+        if low == high: 
+            continue
+
+        c_sum = sum(nums[low: high])
+        
+        if c_sum == invalid:
+            smallest, largest = min(nums[low:high]), max(nums[low:high])
+            print("Min + Max = {} + {} = {}".format(smallest, largest, smallest + largest))
+        
+        elif c_sum > invalid:
+            break
