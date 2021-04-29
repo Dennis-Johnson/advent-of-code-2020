f787f659a1ddc6df0255a63735a2a1f0c0b2b9f0
Dennis Johnson <densarjohn@gmail.com>
2021-04-29 16:24:16 +0530

    D16 parta

---
+++ d16/pa.py
@@ -0,0 +1,44 @@
+import re
+
+def isValid(val):
+    # Returns true if val is in the range of any field
+    for field in fields:
+        if val >= field[0] and val <= field[1] or val >=field[2] and val <= field[3]:
+            return True
+    return False
+
+
+#fieldLines = open('fieldsTest.txt', 'r').readlines()
+fieldLines = open('fields.txt', 'r').readlines()
+fields = []
+
+for line in fieldLines:
+    matchObj = re.findall('(\d+)', line)
+    
+    if len(matchObj) != 4:
+        print("Error invalid line")
+        break
+
+    fields.append([int(x) for x in matchObj])
+
+
+print(fields)
+
+
+errorRate = 0
+#ticketLines = open('nearbyTest.txt', 'r').readlines()
+ticketLines = open('nearbyTickets.txt', 'r').readlines()
+
+for line in ticketLines:
+    values = line.split(",")
+    values = [int(x) for x in values]
+    
+    for value in values:
+        if not isValid(value):
+            print("Invalid value found {}".format(value))
+            errorRate += value
+
+print("Error rate: {}".format(errorRate))
+
+
+
